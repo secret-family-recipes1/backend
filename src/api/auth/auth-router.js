1032a56e96ac775b445a9534f1d2aee405a1c2a7
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-08-22 14:22:54 -0700

    set up log in functionality

--- src/api/auth/auth-router.js
+++ src/api/auth/auth-router.js
@@ -18,4 +18,20 @@ router.post('/register', (req, res) => {
   });
 });
 
+router.post('/login', (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  Users.findBy({ email: email }).then(([user]) => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        message: `signed in successfully, user: ${user.firstname} ${user.lastname}`,
+      });
+    } else {
+      res
+        .status(401)
+        .json({ message: `invalid username or password` });
+    }
+  });
+});
+
 module.exports = router;
--- src/api/users/user-model.js
+++ src/api/users/user-model.js
@@ -2,8 +2,13 @@ const db = require('../../db/db-config');
 
 module.exports = {
   addUser,
+  findBy,
 };
 
 function addUser(user) {
   return db('users').insert(user);
+}
+
+function findBy(filter) {
+  return db('users').where(filter).orderBy('id');
 }
