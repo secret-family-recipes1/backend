d2eb176dca68a954de503e503d2fd48484fb359c
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-08-24 13:09:49 -0700

    add cors

--- src/api/server.js
+++ src/api/server.js
@@ -8,7 +8,7 @@ const apiRouter = require('./api-router');
 server.use(express.json());
 server.use('/api', cors(), apiRouter);
 server.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
